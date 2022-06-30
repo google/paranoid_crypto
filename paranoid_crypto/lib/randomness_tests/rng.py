23c6cd19057ffb5dc6a0624d51c42cfdf20883ee
Pedro Barbosa <pedroysb@google.com>
2022-06-30 17:09:43 +0200

    Internal change
    PiperOrigin-RevId: 458228523

--- paranoid_crypto/lib/randomness_tests/rng.py
+++ paranoid_crypto/lib/randomness_tests/rng.py
@@ -1,3 +1,16 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """This module implements pseudorandom number generators for testing.
 
 Many of the pseudorandom number generators in this module have serious
@@ -130,8 +143,8 @@ class GmpRand(Rng):
     """Constructs a random number generator using GMP.
 
     Args:
-      output_size: the output size of the LCG. The internal state of the LCG
-         is 2*output_size.
+      output_size: the output size of the LCG. The internal state of the LCG is
+        2*output_size.
     """
     self.output_size = output_size
 
