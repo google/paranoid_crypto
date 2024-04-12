97982f8cb757491c010f952b51d71fc8f9f25fdc
Pedro Barbosa <pedroysb@google.com>
2024-04-12 11:09:50 +0200

    No public description
    PiperOrigin-RevId: 624097515

--- paranoid_crypto/lib/special_case_factoring.py
+++ paranoid_crypto/lib/special_case_factoring.py
@@ -69,7 +69,7 @@ def FactorWithGuess(n: int, p_0: int) -> Optional[list[int]]:
   # To avoid this the cube root of n // 2**(3*shift) is computed instead.
   bits = n.bit_length()
   shift = max(0, (bits // 3) - 52)
-  bound = int((n >> (3 * shift)) ** (1 / 3)) << shift
+  bound = int((int(n) >> (3 * shift)) ** (1 / 3)) << shift
 
   for _, u, v in ntheory_util.ContinuedFraction(p_0, q_0):
     # An approximation u / v of p_0 / q_0 is good enough for this factoring
